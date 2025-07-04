182a905de1bbee8e564f32b678db6407b5911d6d
Guntas Singh <guntas7347@gmail.com>
2025-07-04 15:36:30 +0530

    added login and dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LoginRoutes from "./routes/login.routes";
+import Homepage from "./pages/homepage/homepage";
+import Dashboard from "./pages/dashboard/homepage/main";
 
 const App = () => {
-  return <div>Excel Analytics Platform</div>;
+  return (
+    <div>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="/login/*" element={<LoginRoutes />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
